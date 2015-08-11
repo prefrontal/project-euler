00bbcec61a5c7c0b31cd413e5e67744c3cb82384
Craig Bennett <prefrontal@gmail.com>
2015-08-10 18:33:53 -0700

    Number 15

--- Answers-C++/015.cpp
+++ Answers-C++/015.cpp
@@ -5,11 +5,53 @@
 //
 // How many such routes are there through a 20×20 grid?
 //
-// Answer:
+// Answer: 137846528820
 
 #include <iostream>
 
-int main(int argc, char *argv[]) 
+// This is a Pascal's Triangle problem
+// https://en.wikipedia.org/wiki/Pascal%27s_triangle
+
+namespace
 {
+    const int TARGET_GRID_SIZE = 20;
+}
+
+long long SquarePaths (const int gridSize)
+{
+    // Add one since the first row and column of the square
+    // are the seed values for the actual grid of numbers
+    int depth = gridSize + 1;
     
+    long long square[depth][depth];
+    
+    // Initialize the entire array
+    for (int i = 0; i < depth; i++)
+        for (int j = 0; j < depth; j++)
+            square[i][j] = 0;
+    
+    // Insert first seed row and column
+    for (int k = 0; k < depth; k++)
+    {
+        square[k][0] = 1;
+        square[0][k] = 1;
+    }
+    
+    // Now, calculate the rest of the square
+    for (int m = 1; m < depth; m++)
+    {
+        for (int n = 1; n < depth; n++)
+        {
+            square[m][n] = square[m-1][n] + square[m][n-1];
+        }
+    }
+    
+    return square[depth-1][depth-1];
+}
+
+int main (int argc, char *argv[]) 
+{
+    long long answer = SquarePaths (TARGET_GRID_SIZE);
+    
+    std::cout << "The number of routes is: " << answer << std::endl;
 }
