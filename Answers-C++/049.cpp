04a7c7c4959ba1f37c806aad6a429fea02d9cf62
Craig Bennett <prefrontal@gmail.com>
2015-09-04 18:25:03 -0700

    Update 49

--- Answers-C++/049.cpp
+++ Answers-C++/049.cpp
@@ -12,9 +12,60 @@
 // 
 // Answer:
 
+#include <cmath>
 #include <iostream>
+#include <vector>
+
+namespace
+{
+    const int MAXIMUM_VALUE = 9999;
+}
+
+/*
+ * Sieve of Eratosthenes (integers)
+ * Simple, ancient algorithm for finding all prime numbers up to any given limit.
+ * It does so by iteratively marking as composite (i.e., not prime) the multiples
+ * of each prime, starting with the multiples of 2.
+ *
+ * Provides a vector of integers that represent the prime numbers under the maximum value
+*/
+void GetPrimes (const int maximum, std::vector<int> &primes)
+{
+    // Clear the input vector in case there is existing data
+    primes.clear();
+    
+    // There are no primes less than 2
+    if (maximum < 2)
+        return;
+    
+    // Construct and execute the Sieve
+    const int sqrtMaximum = static_cast<int>( sqrt(static_cast<double>(maximum)) );
+    std::vector<bool> primeTracker (maximum, true);
+    
+    for (int i = 2; i < sqrtMaximum; ++i)
+    {
+        if (!primeTracker[i])
+            continue;
+        
+        for (int j = i + i; j < maximum; j += i)
+        {
+            if (j < 0)  // Guard against integer overflow
+                break;
+            
+            primeTracker[j] = false;
+        }
+    }
+    
+    // Generate the list of primes to return
+    for (int k = 2; k < maximum; ++k)
+    {
+        if (primeTracker[k] == true)
+            primes.push_back (k);
+    }
+}
 
 int main (int argc, char *argv[]) 
 {
-    
+    std::vector<int> primes;
+    GetPrimes (MAXIMUM_VALUE, primes);
 }
