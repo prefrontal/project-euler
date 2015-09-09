171439d81da64ea9ad5fd18a9bc36d4e9e15eaf9
Craig Bennett <prefrontal@gmail.com>
2015-09-09 16:52:29 -0700

    Add 51 and 52

---
+++ Answers-C++/052.cpp
@@ -0,0 +1,16 @@
+// Project Euler Problem 52
+//
+// It can be seen that the number, 125874, and its double, 251748, 
+// contain exactly the same digits, but in a different order.
+//
+// Find the smallest positive integer, x, such that 2
+// x, 3x, 4x, 5x, and 6x, contain the same digits.
+//
+// Answer:
+
+#include <iostream>
+
+int main(int argc, char *argv[]) 
+{
+    
+}
