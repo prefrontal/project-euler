688057cd73b783e448bb2a34c08d1f44fc0dc174
Craig Bennett <prefrontal@gmail.com>
2015-09-08 19:46:27 -0700

    Answer 50

--- Answers-C++/050.cpp
+++ Answers-C++/050.cpp
@@ -11,11 +11,97 @@
 // 
 // Which prime, below one-million, can be written as the sum of the most consecutive primes?
 // 
-// Answer:
+// Answer: 997651
 
+#include <cmath>
 #include <iostream>
+#include <vector>
+
+namespace
+{
+	const int MAXIMUM_VALUE = 1000000;	
+}
+
+void GetPrimes (const int maximum, std::vector<int> &primes)
+{
+	// Clear the input vector in case there is existing data
+	primes.clear();
+	
+	// There are no primes less than 2
+	if (maximum < 2)
+		return;
+	
+	// Construct and execute the Sieve
+	const int sqrtMaximum = static_cast<int>( sqrt(static_cast<double>(maximum)) );
+	std::vector<bool> primeTracker (maximum, true);
+	
+	for (int i = 2; i < sqrtMaximum; ++i)
+	{
+		if (!primeTracker[i])
+			continue;
+		
+		for (int j = i + i; j < maximum; j += i)
+		{
+			if (j < 0)  // Guard against integer overflow
+				break;
+			
+			primeTracker[j] = false;
+		}
+	}
+	
+	// Generate the list of primes to return
+	for (int k = 2; k < maximum; ++k)
+	{
+		if (primeTracker[k] == true)
+			primes.push_back (k);
+	}
+}
+
+// This answer is computationally inefficient. Since we work backward over the primes
+// array we actually find the answer within a few seconds. However, it takes a significant
+// amount of time to go through the entire primes array to know the definitive answer.
+// This could/should be reworked with a another strategy that is more optimal.
 
 int main (int argc, char *argv[]) 
 {
-    
+    std::vector<int> primes;
+	GetPrimes (MAXIMUM_VALUE, primes);
+	
+	// For loop efficieny, cache the size
+	int numPrimes = primes.size();
+	
+	int maxPrimeCount = 0;
+	int maxPrime = 0;
+	
+	// Loop over all primes in the vector in reverse since we assume
+	// our target is a value closer to the top value than the bottom
+	for (int i = numPrimes-1; i >= 0; i--)
+	{
+		// This loop defines the starting point for 
+		// calculating the consecutive sums
+		for (int j = 0; j < numPrimes; j++)
+		{
+			int primeCount = 0;
+			int primeSum = 0;
+			
+			// Calculate the consecutive prime sum
+			while (primeSum < primes[i])
+			{
+				if (j+primeCount >= numPrimes)
+					break;
+					
+				primeSum += primes[j+primeCount];
+				primeCount++;
+			}
+				
+		    // Count it if we hit the prime exactly and have the highest count
+			if ((primeSum == primes[i]) && (primeCount > maxPrimeCount))
+			{
+				maxPrime = primes[i];
+				maxPrimeCount = primeCount;
+			}
+		}
+	}
+	
+	std::cout << "The target prime is: " << maxPrime << std::endl;
 }
