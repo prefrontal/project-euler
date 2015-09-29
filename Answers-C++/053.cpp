9b4ff72c00704b73edc59b9a034dcbcffe0fedd6
Craig Bennett <prefrontal@gmail.com>
2015-09-28 17:18:37 -0700

    Answer 53, start 54

--- Answers-C++/053.cpp
+++ Answers-C++/053.cpp
@@ -14,11 +14,46 @@
 // It is not until n = 23, that a value exceeds one-million: 23C10 = 1144066.
 // How many, not necessarily distinct, values of nCr, for 1 ≤ n ≤ 100, are greater than one-million?
 //
-// Answer:
+// Answer: 4075
 
 #include <iostream>
 
+namespace
+{
+    const int UPPER_NCR_LIMIT = 100;
+    const int UPPER_RESULT_LIMIT = 1000000;
+}
+
+// We know that we will overflow a long long int for this question
+// so we are cheating a bit here. Since we later just need to know
+// if it is greater than an upper limit, we can hold the huge number 
+// in a double knowing that it will work for the final comparison.
+
+// Calculate the factorial of an input integer
+// Return the value as a double and not a long long integer
+double factorial (const int input) 
+{
+    int temp = input;
+    double result = 1;
+
+    while (temp > 1) 
+        result *= temp--;
+    
+    return result;
+}
+
 int main (int argc, char *argv[]) 
 {
-    
+    int finalCount = 0;
+     
+    for (int n = 1; n <= UPPER_NCR_LIMIT; n++) 
+    {
+        for (int r = 0; r <= n; r++) 
+        {
+            if (factorial(n) / (factorial(r)*factorial(n-r)) >= UPPER_RESULT_LIMIT)
+                   finalCount++;
+        }
+    }
+
+    std::cout << "The final count is: " << finalCount << std::endl;
 }
