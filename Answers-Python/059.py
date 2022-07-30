1546ed6f12ccef1f6e0300aedbe26b73be87b9a6
Craig Bennett <prefrontal@gmail.com>
2022-07-30 15:18:49 -0700

    Answer #59 in Python

--- Answers-Python/059.py
+++ Answers-Python/059.py
@@ -23,4 +23,54 @@
 # that the plain text must contain common English words, decrypt the message
 # and find the sum of the ASCII values in the original text.
 #
-# Answer:
+# Answer: 129448
+
+# The file provided is a single line of text consisting of numbers
+# separated by commas. Each number will represent an ASCII letter once
+# XOR'd against the right encryption key
+
+ASCII_LOWERCASE_START = 97
+ASCII_LOWERCASE_END = 122
+
+
+def load_file():
+    input_file = open('../Data Files/059-Cipher.txt', 'r')
+    text = input_file.read()
+    return text.split(',')
+
+
+def decrypt(message, candidate_key):
+    output = []
+    for i in range(len(message)):
+        key_index = i % len(candidate_key)
+        output.append(int(message[i]) ^ candidate_key[key_index])
+    return output
+
+
+def find_key(message):
+    best_ascii_count = 0
+    best_candidate = ""
+
+    # The target key is a set of three lowercase ASCII characters. We will brute force search
+    # across all combinations, evaluating the output by the number of lowercase ASCII characters
+    # that we find in the decrypted sequence. Highest number wins.
+    for i in range(ASCII_LOWERCASE_START, ASCII_LOWERCASE_END + 1):
+        for j in range(ASCII_LOWERCASE_START, ASCII_LOWERCASE_END + 1):
+            for k in range(ASCII_LOWERCASE_START, ASCII_LOWERCASE_END + 1):
+                candidate = decrypt(message, [i, j, k])
+                num_ascii_chars = len([x for x in candidate if ASCII_LOWERCASE_START <= x <= ASCII_LOWERCASE_END])
+                if best_ascii_count < num_ascii_chars:
+                    print(num_ascii_chars, " - ", [i, j, k])
+                    best_ascii_count = num_ascii_chars
+                    best_candidate = candidate
+
+    return best_candidate
+
+
+# Processing start
+original_message = load_file()
+decrypted_message = find_key(original_message)
+
+# Print results
+print("".join(chr(i) for i in decrypted_message))
+print("Answer: ", sum(decrypted_message))
